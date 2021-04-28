488abbe4ca28db52569d6896be6913d40d7be720
ramen-7 <76044999+ramen-7@users.noreply.github.com>
2021-04-28 13:13:56 +0530

    Eshop

---
+++ Eshop/store/models/product.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .category import Category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    description = models.CharField(max_length=200, default='')
+    image = models.ImageField(upload_to='uploads/Products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
